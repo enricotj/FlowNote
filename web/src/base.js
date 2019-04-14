ee3d845535564068e100e83e50f342e758e2ebcb
enricotj <taiphoon007@gmail.com>
2019-04-14 16:25:06 -0700

    import firebase/app instead of just firebase

--- web/src/base.js
+++ web/src/base.js
@@ -1,5 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
 var config = {
 	apiKey: "AIzaSyBEnBSP2AhyluulVVKVtJhk0uLoAuNJ8SQ",
